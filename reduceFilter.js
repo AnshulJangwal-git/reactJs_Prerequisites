8e89398cfda7cfe95ed2ae3c7f1aa6142a9d2213
anshul <anshuljangwal006@gmail.com>
2021-09-07 02:32:55 +0530

    replica of reduce filter

--- reduceFilter.js
+++ reduceFilter.js
@@ -5,5 +5,21 @@ function sum(a, b){
 }
 
 let reducedValue = a.reduce(sum) ;
-
 console.log(reducedValue) ;
+
+
+//-----------------------------------------
+
+//myReduce
+function myReduce(arr, f){
+    let ans = arr[0] ;
+    //arr = [1,2,3]
+
+    for(let i = 1; i < arr.length; i++){
+        ans = f(ans, arr[i]) ;
+
+    }
+    return ans ;
+}
+
+console.log(myReduce(a, sum)) ;
