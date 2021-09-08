7401c8712b89e0eb9b62e1fb98741a90c3f496cb
anshul <anshuljangwal006@gmail.com>
2021-09-08 09:54:25 +0530

    up

---
+++ ArrowFunctions.js
@@ -0,0 +1,21 @@
+function f(x, y){
+    return 2 * x ;
+}
+
+//Arrow function
+let f = (x, y) => {
+    return 2 * x ;
+}
+
+
+// for 1 Argument
+function f2(x){
+    return 2 * x;
+}
+
+let f2 = x => {
+    return 2 * x ;
+}
+
+// Also if we have only one return statement..
+let f = x => 2 * x ;
