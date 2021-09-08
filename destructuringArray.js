46f520c10d1a90e0a5b84bb051e0bba8f894cb5c
anshul <anshuljangwal006@gmail.com>
2021-09-08 09:34:12 +0530

    up

--- destructuringArray.js
+++ destructuringArray.js
@@ -8,8 +8,14 @@ let a = [1, 2, 3, 4, 5, 6, 7] ;
 //2
 
 //To skip some element 
-let [someVar, nextVar, _, reqVar] = a ;
-console.log(reqVar) ;
-//4
-console.log(_) ;
+// let [someVar, nextVar, _, reqVar] = a ;
+// console.log(reqVar) ;
+// //4
+// console.log(_) ;
 //3
+
+//same can be done by replacing 'underscore' by 'space'
+
+let [n1, n2, _, n3, n4] = a;
+console.log(n4) ;
+console.log(_) ;
